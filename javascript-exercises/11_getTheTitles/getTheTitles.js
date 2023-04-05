99b229e93167f0521ba6b2e56327c03bd122f790
Raffy <raffyjay_dayag@yahoo.com>
2023-04-06 01:16:59 +0800

    finished javascript30 exercise up to 12

--- javascript-exercises/11_getTheTitles/getTheTitles.js
+++ javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    titles = [];
+    for (let i = 0; i < books.length; i++) {
+        title = books[i].title;
+        titles.push(title)
+    }
+    return titles;
 };
 
 // Do not edit below this line
--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = '';
+    let age = 0;
+    const year = new Date().getFullYear()
+    for (let i = 0; i < people.length; i++){
+        // console.log(people[i].name)
+        if (!people[i].hasOwnProperty('yearOfDeath')){
+            currentAge = year - people[i].yearOfBirth;
+        } else {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        if (currentAge > age){
+            age = currentAge;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
+
+// for (let i = 0; i < people.length; i++){
+//     if ((people[i].yearOfDeath - people[i].yearOfBirth) > age){
+//         age = people[i].yearOfDeath - people[i].yearOfBirth;
+//         oldest = people[i]
+//     }
+// }
+// return oldest;
 
 // Do not edit below this line
 module.exports = findTheOldest;
